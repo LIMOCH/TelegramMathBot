722496a0620007e349f58b10d50dde40a114fd78
LIMOCH <102480515+LIMOCH@users.noreply.github.com>
2023-02-01 19:50:31 +0600

    Add files via upload
    this program can calculate the perimeter of a rectangle

--- tag.py
+++ tag.py
@@ -18,12 +18,13 @@ TOKEN = "5836886917:AAFCzT6SGLfY9Q34xWnvi03l7Ad25fOxrJI"
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-inline_btn_psabr = InlineKeyboardButton(emojize('Вычисление площади фигуры'), callback_data='psabr')
-
+inline_btn_psabr = InlineKeyboardButton(emojize('Вычисление площади, периметра, длинны, ширины фигуры'), callback_data='psabr')
+inline_btn_vstr = InlineKeyboardButton(emojize('Вычисление скорости, расстояния, времени'), callback_data='vstr')
 
 inline_kb = InlineKeyboardMarkup()# Создали и загрузили инлайн клаву
-inline_kb.add(inline_btn_psabr)
+inline_kb.add(inline_btn_psabr, inline_btn_vstr)
 
+#===================Площадь==================================
 inline_btn_p = InlineKeyboardButton(emojize('Периметр'), callback_data='p')
 inline_btn_s = InlineKeyboardButton(emojize('Площадь'), callback_data='s')
 inline_btn_a = InlineKeyboardButton(emojize('Длинна'), callback_data='a')
@@ -31,6 +32,13 @@ inline_btn_b = InlineKeyboardButton(emojize('Ширина'), callback_data='b')
 
 inline_ssf = InlineKeyboardMarkup()# Создали и загрузили инлайн клаву
 inline_ssf.add(inline_btn_p,inline_btn_s,inline_btn_a,inline_btn_b)
+#=============================================================
+#===================VST==========================================
+inline_btn_v = InlineKeyboardButton(emojize('Скорость'), callback_data='v')
+inline_btn_t = InlineKeyboardButton(emojize('Время'), callback_data='t')
+inline_btn_s = InlineKeyboardButton(emojize('Расстояние'), callback_data='s')
+inline_ssd = InlineKeyboardMarkup()# Создали и загрузили инлайн клаву
+inline_ssd.add(inline_btn_v,inline_btn_t,inline_btn_s)
 
 
 @dp.message_handler(commands=['start'])
@@ -40,12 +48,18 @@ async def process_start_command(message: types.Message):
 
 @dp.callback_query_handler(lambda c: c.data == 'psabr', state=None)
 async def sheessh(message: types.Message):
-    await bot.send_message(message.from_user.id, "Выбрать, что вам надо найти? (Периметр, Площадь, Длинна, Ширина) - ", reply_markup=inline_ssf)
+    await bot.send_message(message.from_user.id, "Выбрать, что вам надо найти? (Периметр, Площадь, Длинна, Ширина)", reply_markup=inline_ssf)
+
+@dp.callback_query_handler(lambda c: c.data == 'vst', state=None)
+async def sheessh(message: types.Message):
+    await bot.send_message(message.from_user.id, "Выбрать, что вам надо найти? (Скорость(м/c), время(с) путь(м))", reply_markup=inline_ssd)
 
 class Form(StatesGroup):
     a = State() 
     b = State() 
 
+
+#====================Периметр=================================
 @dp.callback_query_handler(lambda c: c.data == 'p')
 async def sheesh(message: types.Message, state: FSMContext):
     await bot.send_message(message.from_user.id, "Введите длинну: ")
@@ -53,24 +67,98 @@ async def sheesh(message: types.Message, state: FSMContext):
 
 @dp.message_handler(state=Form.a)
 async def process_name(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['a'] = int(message.text)
 
     await Form.next()
     await message.reply("Введите ширину:")
     await state.update_data(a=int(message.text))
-    #await bot.send_message(message.from_user.id, "Введите ширину: ")
-    #await Form.a.set()
-    #ab = Form()
-    #P = 2 * (ab.a + ab.b)
-    #await bot.send_message(message.from_user.id, 'Периметр = ',P)
-
 @dp.message_handler(state=Form.b) 
 async def namee(message: types.Message, state: FSMContext):
-    await state.update_data(b=int(message.text))
-    b = message.text
-    ab = Form()
-    p = 2 * (ab.a + ab.b)
-    await bot.send_message(message.from_user.id, text=f'Спасибо за ответ\n '
-                                                  f'Тебя зовут: {p}')
+    async with state.proxy() as data:
+        await state.update_data(b=int(message.text))
+        data['b'] = int(message.text)
+        p = 2 * (data['a'] + data['b'])
+    await bot.send_message(message.from_user.id, text=f'Периметр = {p}')
+    await state.finish()
+
+
+
+@dp.callback_query_handler(lambda c: c.data == 's')
+async def sheeesh(message: types.Message, state: FSMContext):
+    await bot.send_message(message.from_user.id, "Введите длинну: ")
+    await Form.a.set()
+
+@dp.message_handler(state=Form.a)
+async def processs_name(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['a'] = int(message.text)
+
+    await Form.next()
+    await message.reply("Введите ширину:")
+    await state.update_data(a=int(message.text))
+
+@dp.message_handler(state=Form.b) 
+async def nameee(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        await state.update_data(b=int(message.text))
+        data['b'] = int(message.text)
+        s = data['a'] * data['b']
+    await bot.send_message(message.from_user.id, text=f'Площадь = {p}')
+    await state.finish()
+
+
+
+@dp.callback_query_handler(lambda c: c.data == 'a')
+async def sheeshas(message: types.Message, state: FSMContext):
+    await bot.send_message(message.from_user.id, "Введите площадь: ")
+    await Form.a.set()
+
+@dp.message_handler(state=Form.a)
+async def process_nameas(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['a'] = int(message.text)
+
+    await Form.next()
+    await message.reply("Введите ширину:")
+    await state.update_data(a=int(message.text))
+
+@dp.message_handler(state=Form.b) 
+async def nameeds(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        await state.update_data(b=int(message.text))
+        data['b'] = int(message.text)
+        s = data['a'] / data['b']
+    await bot.send_message(message.from_user.id, text=f'Длинна = {p}')
+    await state.finish()
+
+
+@dp.callback_query_handler(lambda c: c.data == 'b')
+async def sheeshas(message: types.Message, state: FSMContext):
+    await bot.send_message(message.from_user.id, "Введите длинну: ")
+    await Form.a.set()
+
+@dp.message_handler(state=Form.a)
+async def process_namdse(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['a'] = int(message.text)
+
+    await Form.next()
+    await message.reply("Введите площадь:")
+    await state.update_data(a=int(message.text))
+
+@dp.message_handler(state=Form.b) 
+async def namease(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        await state.update_data(b=int(message.text))
+        data['b'] = int(message.text)
+        s = data['b'] / data['a']
+    await bot.send_message(message.from_user.id, text=f'Длинна = {p}')
+    await state.finish()
+#=============================================================
+
+
+#======================VST===================================
 
 
 if __name__ == '__main__':
